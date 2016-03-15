2af8987dd304db8cfeac374648bed2f415dfb7ce
hechen <me@hechen0.com>
2016-03-15 13:05:16 +0800

    extract tag and answer

--- zhihu.go
+++ zhihu.go
@@ -21,7 +21,36 @@ type Question struct {
 	Title      string
 	AnswerNum  int
 	CommentNum int
-	Tags       map[string]string
+	Tags       []int
+
+	UpdatedAt int64
+}
+
+type Tag struct {
+	Id          int
+	Url         string
+	Title       string
+	Description string
+
+	UpdatedAt int64
+}
+
+type Answer struct {
+	Id           int
+	QuestionId   int
+	AnswerId     int
+	Url          string
+	Vote         int
+	Author       string
+	AnswerLength int
+	CommentNum   int
+
+	CreatedAt int64
+	UpdatedAt int64
+}
+
+type Selector struct {
+	Id int
 }
 
 var (
@@ -30,7 +59,7 @@ var (
 	url = flag.String("url", "", "mongodb url")
 	db  = flag.String("db", "zhihu", "database")
 
-	regexAnsNum = regexp.MustCompile("^(.*) (.+)$")
+	regexCommentNum = regexp.MustCompile("^([0-9]{1}[0-9]+)(\\s+)(.+)$")
 )
 
 func main() {
@@ -39,6 +68,8 @@ func main() {
 
 	question_start := *s
 	question_end := *e
+
+	start_time := time.Now()
 
 	if question_start == 0 || question_end == 0 || question_start > question_end {
 		fmt.Println("question start and end must set correctly")
@@ -69,6 +100,9 @@ func main() {
 	}
 
 	wg.Wait()
+
+	elasp := time.Since(start_time)
+	fmt.Printf("\n\n from %d to %d, crawl %d ; time usage %f min (%f seconds) \n", question_start, question_end, question_end-question_start, elasp.Minutes(), elasp.Seconds())
 }
 
 func run_worker(start, end, crawler int) {
@@ -77,8 +111,9 @@ func run_worker(start, end, crawler int) {
 	check(err)
 	defer session.Close()
 
-	fmt.Println(*url)
-	client := session.DB("zhihu").C("question")
+	question_collection := session.DB("zhihu").C("question")
+	tag_collection := session.DB("zhihu").C("tag")
+	answer_collection := session.DB("zhihu").C("answer")
 
 	timer := time.Tick(5 * time.Second)
 
@@ -110,11 +145,26 @@ func run_worker(start, end, crawler int) {
 		if resp.StatusCode == 200 {
 
 			question := extract_question(i, doc)
-
-			fmt.Println(question)
-
-			err := client.Insert(&question)
+			_, err := question_collection.Upsert(&Selector{question.Id}, question)
 			check(err)
+
+			//extract_tags(i, doc)
+			tags := extract_tags(i, doc)
+
+			for i := range tags {
+				tag := tags[i]
+				_, err := tag_collection.Upsert(&Selector{tag.Id}, tag)
+				check(err)
+			}
+
+			answers := extract_answers(i, doc)
+			//extract_answers(i, doc)
+
+			for i := range answers {
+				answer := answers[i]
+				_, err := answer_collection.Upsert(&Selector{answer.Id}, answer)
+				check(err)
+			}
 
 			fmt.Printf("%s %s\n", question.Url, question.Title)
 
@@ -139,17 +189,109 @@ func extract_question(question_id int, doc *goquery.Document) *Question {
 	answer_num, _ := strconv.Atoi(tmp_str)
 
 	tmp_str = doc.Find("div#zh-question-meta-wrap .meta-item").First().Text()
-	fields := regexAnsNum.FindStringSubmatch(tmp_str)
-	fmt.Printf("%v \n", fields)
-	comment_num, _ := strconv.Atoi(strings.Split(tmp_str, " ")[0])
+	comment_num := extract_comment_num(tmp_str)
 
-	tags := make(map[string]string)
+	var tags []int
+	doc.Find("a.zm-item-tag").Each(func(i int, s *goquery.Selection) {
+		tmp_str, exists := s.Attr("href")
+		if exists {
+			tag_id, err := strconv.Atoi(strings.Split(tmp_str, "/")[2])
+			if err == nil {
+				tags = append(tags, tag_id)
+			}
+		}
+	})
 
-	return &Question{id, url, title, answer_num, comment_num, tags}
+	now := time.Now().Unix()
+
+	return &Question{id, url, title, answer_num, comment_num, tags, now}
+}
+
+func extract_tags(question_id int, doc *goquery.Document) []*Tag {
+	var tags []*Tag
+
+	doc.Find("a.zm-item-tag").Each(func(i int, s *goquery.Selection) {
+		tag, exists := extract_tag(question_id, s)
+		if exists {
+			tags = append(tags, tag)
+		}
+	})
+
+	return tags
+}
+
+func extract_tag(question_id int, s *goquery.Selection) (tag *Tag, exists bool) {
+	title := trim(s.Text())
+	url, exists := s.Attr("href")
+	if exists {
+		url = trim(url)
+		tag_id, _ := strconv.Atoi(strings.Split(url, "/")[2])
+		tag = &Tag{tag_id, "https://www.zhihu.com" + url, title, "", time.Now().Unix()}
+	}
+	return
+}
+
+func extract_answers(question_id int, doc *goquery.Document) []*Answer {
+	var answers []*Answer
+
+	doc.Find("div.zm-item-answer").Each(func(i int, s *goquery.Selection) {
+		answers = append(answers, extract_answer(question_id, s))
+	})
+
+	return answers
+}
+
+func extract_answer(question_id int, s *goquery.Selection) (answer *Answer) {
+	var tmp_str string
+
+	tmp_str, _ = s.Attr("data-aid")
+	id, _ := strconv.Atoi(tmp_str)
+
+	tmp_str, _ = s.Attr("data-atoken")
+	answer_id, _ := strconv.Atoi(tmp_str)
+
+	url, _ := s.Find(".answer-date-link").Attr("href")
+	tmp_str, _ = s.Find(".zm-item-vote-info").Attr("data-votecount")
+	vote, _ := strconv.Atoi(tmp_str)
+	author, exists := s.Find(".author-link").Attr("href")
+
+	if exists {
+		author = author[8:]
+	} else {
+		author = "ano"
+	}
+
+	answer_length := len(s.Find(".zm-editable-content").Text())
+
+	tmp_str = s.Find(".meta-item.toggle-comment").Text()
+	comment_num := extract_comment_num(tmp_str)
+
+	tmp_str, _ = s.Attr("data-created")
+	created_at, _ := strconv.ParseInt(tmp_str, 10, 64)
+
+	now := time.Now().Unix()
+
+	answer = &Answer{id, question_id, answer_id, "https://www.zhihu.com" + url, vote, author, answer_length, comment_num, created_at, now}
+	return
+}
+
+func extract_comment_num(str string) (num int) {
+	str = trim(str)
+	fields := regexCommentNum.FindStringSubmatch(str)
+	if len(fields) < 2 {
+		num = 0
+	} else {
+		num, _ = strconv.Atoi(fields[1])
+	}
+	return
 }
 
 func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func trim(str string) string {
+	return strings.Trim(str, " \r\n")
 }
