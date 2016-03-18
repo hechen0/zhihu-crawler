9b18e20f0ab84b6010c3e544423455e732a7e22d
hechen <me@hechen0.com>
2016-03-18 14:55:53 +0800

    hehe

--- auto.py
+++ auto.py
@@ -7,10 +7,10 @@ def main():
   total = len(files)
   for i in range(total):
     j = i + 1
-    if j % 9 == 0:
+    if j % 6 == 0:
       cmd = "git add %s %s %s %s %s %s; git commit -m 'left %s'; git push;" % (files[i-5], files[i-4], files[i-3], files[i-2], files[i-1], files[i], total - i)
       subprocess.call(cmd, shell=True)
-      sleep(.5)
+      sleep(.3)
 
 if __name__ == "__main__":
 	main()
