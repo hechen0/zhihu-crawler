0a55040e4a3dd1b9915084c5cdd58946e1ad7bc6
hechen <me@hechen0.com>
2016-03-18 14:06:13 +0800

    update auto.py

--- auto.py
+++ auto.py
@@ -2,13 +2,13 @@ from time import sleep
 import subprocess
 
 def main():
-  files_to_commit = subprocess.check_output("git ls-files . --others --exclude-standard", shell=True).strip().split("\n")
+  files = subprocess.check_output("git ls-files . --others --exclude-standard", shell=True).strip().split("\n")
 
-  total = len(files_to_commit)
+  total = len(files)
   for i in range(total):
     j = i + 1
-    if j % 3 == 0:
-      cmd = "git add %s %s %s; git commit -m 'left %s'; git push;" % (files_to_commit[i - 2], files_to_commit[i-1], files_to_commit[i], total - i)
+    if j % 5 == 0:
+      cmd = "git add %s %s %s %s %s; git commit -m 'left %s'; git push;" % (files[i-4], files[-3], files[i - 2], files[i-1], files[i], total - i)
       subprocess.call(cmd, shell=True)
       sleep(.5)
 
